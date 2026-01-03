7f14eb648a309d81e716d5cd02fd528a26e2dc2f
Bongo <bongopoyo@proton.me>
2026-01-03 10:02:18 +0500

    Fixed some issues

--- source/sprite.hpp
+++ source/sprite.hpp
@@ -2,12 +2,12 @@
 #include <raylib.h>
 
 enum class Type {
-    Pawn   = 1,
-    Rook   = 2,
-    Knight = 3,
-    Bishop = 4,
-    Queen  = 5,
-    King   = 6
+    Pawn   = 0,
+    Rook   = 1,
+    Knight = 2,
+    Bishop = 3,
+    Queen  = 4,
+    King   = 5
 };
 
 struct Sprite2D {
