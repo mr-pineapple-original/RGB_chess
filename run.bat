3ce2d62f1a75ec50e7d09552521902569cdf5680
Bongo <bongopoyo@proton.me>
2026-01-03 10:28:43 +0500

    run.bat fixed

--- run.bat
+++ run.bat
@@ -1,19 +1,20 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-REM Compiler
 set CXX=g++
-
-REM Paths
-set SRC=source
+set SRC_DIR=source
 set OUT=source\main.exe
 
-REM Raylib paths (MSYS2 ucrt64)
 set INCLUDE=C:\msys64\ucrt64\include
 set LIB=C:\msys64\ucrt64\lib
 
-REM Compile all cpp files in source/
-%CXX% -g %SRC%\*.cpp ^
+set SOURCES=
+
+for %%f in (%SRC_DIR%\*.cpp) do (
+    set SOURCES=!SOURCES! %%f
+)
+
+%CXX% -g %SOURCES% ^
     -I"%INCLUDE%" ^
     -L"%LIB%" ^
     -lraylib -lopengl32 -lgdi32 -lwinmm ^
